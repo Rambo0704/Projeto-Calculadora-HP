4944a9cf628e1c8f4af4a7245a8ac6492c52f6dc
Gabriel Rambo <gabrielantoniorx@gmail.com>
2025-06-09 20:36:26 -0300

    fix getFormaInFixa

--- calculadora.c
+++ calculadora.c
@@ -5,33 +5,22 @@
 #include <string.h> 
 
 
-#define MAX_EXPR_SIZE 512
-#define STACK_DEPTH 50 
-
+#define max_express 512
+#define max_itens 50 
 typedef struct {
-    char items[STACK_DEPTH][MAX_EXPR_SIZE]; 
+    char items[max_itens][max_express]; 
     int top;
 } Pilha_Caracter;
-
-
-void initPilha_Caracter(Pilha_Caracter *s) {
+void CriarPilha_Caracter(Pilha_Caracter *s) {
     s->top = -1;
 }
-
-
-bool verificarPilha_Caracter(Pilha_Caracter *s) {
-    return s->top == -1;
-}
-
-
 void InserirChar(Pilha_Caracter *s, const char* value) {
-    if (s->top >= STACK_DEPTH - 1) {
+    if (s->top >= max_itens - 1) {
         printf("Erro: Estouro da pilha de strings!\n");
         exit(1);
     }
     strcpy(s->items[++(s->top)], value);
 }
-
 
 void RetirarChar(Pilha_Caracter *s, char* dest) {
     if (verificarPilha_Caracter(s)) { 
@@ -41,66 +30,50 @@ void RetirarChar(Pilha_Caracter *s, char* dest) {
     strcpy(dest, s->items[(s->top)--]);
 }
 
-
-
-char *getFormaInFixa(char *Str) {
+char* getFormaInFixa(char *Str) {
     Pilha_Caracter pilha;
     initPilha_Caracter(&pilha);
     
-    
-    static char finalResult[MAX_EXPR_SIZE];
+    static char finalResult[max_express];
 
-    
-    char strCopy[MAX_EXPR_SIZE];
-    strcpy(strCopy, Str);
-
-    char *token = strtok(strCopy, " ");
+    char *token = strtok(Str, " ");
     
     while (token != NULL) {
-        
         if (strcmp(token, "+") != 0 && strcmp(token, "-") != 0 &&
             strcmp(token, "*") != 0 && strcmp(token, "/") != 0 &&
             strcmp(token, "%") != 0 && strcmp(token, "^") != 0 &&
             strcmp(token, "sen") != 0 && strcmp(token, "cos") != 0 &&
             strcmp(token, "tg") != 0 && strcmp(token, "log") != 0 &&
-            strcmp(token, "raiz") != 0)
+            strcmp(token, "raiz") != 0) 
         {
-            
             InserirChar(&pilha, token);
         }
-        
         else if (strcmp(token, "raiz") == 0 || strcmp(token, "sen") == 0 ||
                  strcmp(token, "cos") == 0 || strcmp(token, "tg") == 0 ||
-                 strcmp(token, "log") == 0)
+                 strcmp(token, "log") == 0) 
         {
-            char operando1[MAX_EXPR_SIZE];
-            
+            char operando1[max_express];
             RetirarChar(&pilha, operando1);
 
-            char novaExpr[MAX_EXPR_SIZE];
+            char novaExpr[max_express];
             sprintf(novaExpr, "%s(%s)", token, operando1);
-            
             InserirChar(&pilha, novaExpr);
-        }
-        
-        else
+        } 
+        else 
         {
-            char operando2[MAX_EXPR_SIZE];
-            char operando1[MAX_EXPR_SIZE];
-            
+            char operando2[max_express];
+            char operando1[max_express];
             RetirarChar(&pilha, operando2);
             RetirarChar(&pilha, operando1);
 
-            char novaExpr[MAX_EXPR_SIZE];
+            char novaExpr[max_express];
             sprintf(novaExpr, "(%s %s %s)", operando1, token, operando2);
-           
             InserirChar(&pilha, novaExpr);
         }
 
         token = strtok(NULL, " ");
     }
 
-    
     RetirarChar(&pilha, finalResult);
     return finalResult;
 }
--- main.c
+++ main.c
@@ -1,1 +1,13 @@
-printf("Hello World");
+#include <stdio.h>
+#include <stdlib.h> 
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h> 
+#include "calculadora.h"
+
+int main(){
+    char *result = getFormaInFixa("7 2 * 4 +");
+    printf("Expressao em forma infixa: %s\n", result);
+
+    return 0;
+}
